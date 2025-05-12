0e801b3f1908cbec976e2850e7b08a81b3cbd2c0
Mjawadi2002 <mouhanned.j18@gmail.com>
2025-05-12 11:37:47 +0100

    finished containerizaton

--- dev/back/server.js
+++ dev/back/server.js
@@ -17,14 +17,14 @@ const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["http://localhost", "http://localhost:80", "http://frontend"],
     methods: ["GET", "POST"],
   },
 });
 
 // Middlewares
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:3001"],
+  origin: ["http://localhost", "http://localhost:80", "http://frontend"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
