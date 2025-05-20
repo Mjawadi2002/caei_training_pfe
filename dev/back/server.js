63b387a9749389dea763a60ed1276c1cec5df4e0
Mjawadi2002 <mouhanned.j18@gmail.com>
2025-05-20 13:43:16 +0100

    added jenkins file

--- dev/back/server.js
+++ dev/back/server.js
@@ -17,14 +17,14 @@ const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost", "http://localhost:80", "http://frontend","http://localhost:3000"],
+    origin: ["http://localhost", "http://localhost:80", "http://frontend","http://localhost:3000","http://localhost:3001"],
     methods: ["GET", "POST"],
   },
 });
 
 // Middlewares
 app.use(cors({
-  origin: ["http://localhost", "http://localhost:80", "http://frontend","http://localhost:3000"],
+  origin: ["http://localhost", "http://localhost:80", "http://frontend","http://localhost:3000","http://localhost:3001"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
